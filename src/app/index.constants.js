42034b9f05ddf0d362cefb1e3890876c883dbfe2
art-grig <artem3.1416@gmail.com>
2017-04-03 16:41:13 +0400

    fixes

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -5,6 +5,8 @@
   angular
     .module('newsScraperRc')
     .constant('malarkey', malarkey)
-    .constant('moment', moment);
+    .constant('moment', moment)
+  .constant('API_URL', 'http://mserver:8082/api');
+  //  .constant('API_URL', 'http://10.100.100.156:12135/api');
 
 })();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,14 +3,31 @@
 
   angular
     .module('newsScraperRc')
-    .controller('MainController', MainController);
+    .controller('MainController', MainController)
+    .directive('gridLoading', function () {
+      return {
+        restrict: 'C',
+        require: '^uiGrid',
+        link: function ($scope, $elm, $attrs, uiGridCtrl) {
+          $scope.grid = uiGridCtrl.grid;
+        }
+      }
+    });
+
 
   /** @ngInject */
-  function MainController($scope, $http, $uibModal, RowEditor, uiGridConstants) {
+  function MainController($scope,$timeout, $http, $uibModal, RowEditor, uiGridConstants, API_URL) {
     var vm = this;
 
+    $scope.updateStatus = function(entity) {
+      console.log('test');
+      var reqDest = API_URL + '/news/' + entity.id + '/status/' + entity.status;
+      $http.post(reqDest, "").success(function(response) {console.log(response)})
+        .error(function(response) {  console.log(response); });
+    }
 
     vm.serviceGrid = {
+      rowHeight: 50,
       enableRowSelection : true,
       enableRowHeaderSelection: false,
       enableColumnMenus: false,
@@ -19,16 +36,24 @@
       enableSorting : true,
       enableFiltering : true,
       enableGridMenu : true,
-      minRowsToShow: 25
+      minRowsToShow: 15
     };
+
+    vm.statusTypes = [
+      {value: 1, label: 'Նոր'},
+      {value: 2, label: 'Դիտված'},
+      {value: 3, label: 'Անպետք'}
+    ];
 
     vm.serviceGrid.columnDefs = [
       {
         field: 'index',
-        displayName: '#',
-        enableFiltering : false,
+        displayName: '№',
+        enableSorting: false,
+        enableFiltering: false,
+        enableCellEdit: false,
         width: 50,
-        cellTemplate: '<span>{{rowRenderIndex + 1}}</span>'
+        cellTemplate: '<div class="ui-grid-cell-contents">{{grid.renderContainers.body.visibleRowCache.indexOf(row) + 1}}</div>'
       },
       {
       field: 'title',
@@ -48,13 +73,43 @@
         enableSorting: true,
         enableCellEdit: false,
         cellTemplate: '<span ng-bind="row.entity.createdDate | date: \'MM-dd-yyyy HH:mm:ss\'"></span>'
+      },
+      {
+        field: 'keywords',
+        displayName: 'Բանալիներ',
+        enableSorting: true,
+        enableCellEdit: false,
+        cellTemplate: '<textarea  disabled style=" width:100%" ng-bind="row.entity.keywords.join(\',\')"></textarea>'
+      },
+      {
+        field: 'status',
+        displayName: 'Կարգավիճակ',
+        filter: { selectOptions: vm.statusTypes, defaultValue: 1,type: uiGridConstants.filter.SELECT },
+        enableCellEdit: false,
+        width: 100,
+        // cellTemplate: '<div>{{COL_FIELD == 1 ? "Նոր" :  COL_FIELD == 2 ? "Դիտված" : "Անպետք"}}</div>'
+        cellTemplate: '<select style=" width:100%" class="input" ng-change="grid.appScope.updateStatus(row.entity)" ng-model="row.entity.status" ng-options="v.id as v.name for v in [{id: 1, name:\'Նոր\'}, {id: 2, name: \'Դիտված\'},{id: 3, name: \'Անպետք\'}]"> </select>'
+      },
+      {
+        field: 'concurrences',
+        displayName: '#',
+        enableSorting: true,
+        enableFiltering: false,
+        enableCellEdit: false,
+        width: 50,
+        cellTemplate: '<span>{{row.entity.keywords.length}}</span>'
       }
+
     ];
 
-    $http.get('http://mserver:8082/api/news').success(function(response) {
-      console.log(response);
-      vm.serviceGrid.data = response;
-    });
+    vm.serviceGrid.columnDefs[5].filter.term = 1;
+    $timeout(function () {
+      $http.get(API_URL + '/news').success(function(response) {
+        console.log(response);
+        vm.serviceGrid.data = response;
+      });
+    }, 1000);
+
 
   }
 })();
